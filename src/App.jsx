b22e69cac0289d48df5f29a5df2da4cfad9b3753
Talha1266 <122205374+Talha1266@users.noreply.github.com>
2026-07-29 11:29:45 +0500

    Move OneSignal init to index.html for robustness

--- src/App.jsx
+++ src/App.jsx
@@ -46,21 +46,6 @@ const ProtectedRoute = ({ children }) => {
 };
 
 function App() {
-  useEffect(() => {
-    const initOneSignal = async () => {
-      try {
-        await OneSignal.init({
-          appId: '14080f6b-a747-488a-a947-0acfb6b61cd6',
-          notifyButton: { enable: true },
-          allowLocalhostAsSecureOrigin: true,
-        });
-      } catch (err) {
-        console.error("OneSignal Init Error:", err);
-      }
-    };
-    initOneSignal();
-  }, []);
-
   return (
     <BrowserRouter>
       <AuthListener>
